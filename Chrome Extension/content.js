9905b5a339a64089010616e1c46b5df43688ac6d
Nick <admin@kyuu.moe>
2020-11-21 17:22:10 +0100

    Added Button Styling

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -3,7 +3,6 @@
 
 'use strict';
 
-//var urlRegex = /^https?:\/\/(?:[^./?#]+\.)?github\.com/;
 var fullURL = window.location.href;
 
 // check if on github.com and a pull request, do Background tasks
@@ -13,6 +12,6 @@ if (fullURL.includes("/pull/"))
 
 	for(var i = 0; i < comments.length; i++)
 	{
-		comments[i].innerHTML += "<p><button>SEND TEST</button>";
+		comments[i].innerHTML += "<p><button class=\"btn btn-sm btn-secondary m-0 ml-0 ml-md-2\" >SEND TEST</button>";
 	}
 }
