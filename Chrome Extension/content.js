677e91abc8b96d5a88bc5e45b27d9f336e192398
Jan-Erik Wieczorek <janerwi@aol.com>
2020-11-22 19:42:57 +0100

    unified

--- Chrome Extension/content.js
+++ Chrome Extension/content.js
@@ -2,29 +2,17 @@
 // Content injection and so on
 
 //'use strict';
-//import TruffleContract from 'truffle-contract'
-//import Counter from './../Solidity/build/contracts/Counter.json'
-//import web3 from './web3.min.js'
-//const providerChain = 'https://sokol.poa.network';
-//const web3 = new Web3(providerChain);
-//const counterContract = new TruffleContract(Counter);
-//counterContract.setProvider(providerChain);
-//let accounts = await web3.eth.accounts;
-//print(accounts)
-//const {contract_abi} = require('./../Solidity/build/contracts/Counter.json');
-//let contract_address = '0x4bC69694000cb26ee2a0d09b68c4B911ec778D6E';
-//const contract = web3.eth.Contract(contract_abi, contract_address);
-
-
-// let web3Script = document.createElement("script");
-// let customScript = document.createElement("script");
-// web3Script.src = "https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js";
-// //customScript.src = "./web3.min.js"
-// let head = document.getElementsByTagName("body");
-// head[0].appendChild(web3Script);
-// //head[0].appendChild(customScript);
 
 window.addEventListener('load', () => {
+	/* -------------------------------------------------------------------------------------------
+*                                   Check for injected web3
+------------------------------------------------------------------------------------------- */
+	const provider = 'https://sokol.poa.network';
+	let web3 = linkWeb3(provider);
+	let contract = linkContract(web3);
+	/* -------------------------------------------------------------------------------------------
+    *                                   web3 Contract Setup
+    ------------------------------------------------------------------------------------------- */
 
 /* -------------------------------------------------------------------------------------------
 *                                   inject buttons on PR page
@@ -42,83 +30,11 @@ for(let i = 0; i < comments.length; i++)
 	comments[i].appendChild(btnArr[i]);
 }
 
-
-
-/* -------------------------------------------------------------------------------------------
-*                                   Check for injected web3
-------------------------------------------------------------------------------------------- */
-
-let web3;
-
-	if (typeof web3 !== 'undefined') 
-	{
-		web3 = new Web3(web3.currentProvider);
-	} 
-	else 
-	{
-		console.log('No web3? You should consider trying MetaMask!');
-		web3 = new Web3(new Web3.providers.HttpProvider('https://cdn.jsdelivr.net/npm/web3@latest/dist/web3.min.js'));
-	}
-	
-
-// if (typeof web3 !== 'undefined') {
-// 	web3js = new Web3(web3.currentProvider);
-// } else {
-// 	// set the provider you want from Web3.providers
-// 	web3js = new Web3(new web3.providers.HttpProvider("http://localhost:8545"));
-// }
-
-//metamask
-//if(typeof window !== 'undefined' && typeof window.web3 !== 'undefined'){
-//	web3js = new Web3(window.web3.currentProvider);
-//	console.log("USING METAMASK");
-//}
-//else{
-//	console.log("ERROR");
-//}
+console.log("CONTRACT");
+console.log(contract);
 
 
 
-
-/* -------------------------------------------------------------------------------------------
-*                                   web3 Contract Setup
-------------------------------------------------------------------------------------------- */
-
-let abi = [
-	{
-		"inputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"constant": false,
-		"inputs": [],
-		"name": "increment",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getCounter",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
-let address = '0x4bC69694000cb26ee2a0d09b68c4B911ec778D6E';
-//web3js = window.web3;
-let contract = new web3.eth.Contract(abi, address);
 
 });
 
@@ -126,6 +42,57 @@ let contract = new web3.eth.Contract(abi, address);
 /* -------------------------------------------------------------------------------------------
 *                                   Functions
 ------------------------------------------------------------------------------------------- */
+function linkWeb3(provider)
+{
+	let createdWeb3;
+	if (typeof web3 !== 'undefined')
+	{
+		createdWeb3 = new Web3(web3.currentProvider);
+	}
+	else
+	{
+		console.log('No web3? You should consider trying MetaMask!');
+		createdWeb3 = new Web3(new Web3.providers.HttpProvider(provider));
+	}
+	return createdWeb3;
+}
+function linkContract(web3)
+{
+	let abi = [
+		{
+			"inputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "constructor"
+		},
+		{
+			"constant": false,
+			"inputs": [],
+			"name": "increment",
+			"outputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "getCounter",
+			"outputs": [
+				{
+					"internalType": "uint256",
+					"name": "",
+					"type": "uint256"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		}
+	];
+	let address = '0x4bC69694000cb26ee2a0d09b68c4B911ec778D6E';
+	return new web3.eth.Contract(abi, address);
+}
 function bruh ()
 {
 	console.log("HJEEEELP");
@@ -135,4 +102,31 @@ function printCounter(){
 	contract.methods.getCounter().call().then(res=>{
 		console.log(res);
 	}).catch(err =>console.log(err));
+}
+function incrementCounter()
+{
+	contract.methods.increment().estimateGas({ from: public_address }).then(gas => {
+
+		const tx = {
+			from: public_address,
+			to: contract_address,
+			gas: gas,
+			data: contract.methods.increment().encodeABI()
+		};
+
+		const signPromise = web3.eth.accounts.signTransaction(tx, private_key);
+
+		signPromise.then((signedTx) => {
+			const sentTx = web3.eth.sendSignedTransaction(signedTx.raw || signedTx.rawTransaction);
+			sentTx.on("receipt", receipt => {
+				console.log(receipt);
+				getCounter()
+			});
+			sentTx.on("error", err => {
+				console.log(err);
+			});
+		}).catch(error => console.log(error));
+	}).catch(error => console.log(error));
+
+	printCounter();
 }
