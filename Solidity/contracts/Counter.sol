c4084fe5d5a8c62b70564a794b72697f72d3d8f3
Jan-Erik Wieczorek <janerwi@aol.com>
2020-12-12 22:34:50 +0100

    'backup'

--- Solidity/contracts/Counter.sol
+++ Solidity/contracts/Counter.sol
@@ -3,11 +3,13 @@ pragma solidity 0.5.16;
 contract Counter {
   mapping(string => Comment) public comments;
   enum Stage{INIT, OPEN, CLOSED}
+
   struct Comment{
     bool exists;
     Stage stage;
     uint up;
     uint down;
+    address creator;
   }
 
   constructor() public {
@@ -24,7 +26,12 @@ contract Counter {
   }
   function createComment(string memory commentId) public {
     if(comments[commentId].exists) return;
-    comments[commentId] = Comment(true, Stage.INIT, 0, 0);
+    comments[commentId] = Comment(true, Stage.INIT, 0, 0, address (this));
+  }
+  function createCommentWithOwner(string memory commentId) public {
+    if(comments[commentId].exists) return;
+    comments[commentId] = Comment(true, Stage.INIT, 0, 0, msg.sender);
+
   }
 
   function getUpvotes(string memory commentId) public view returns (uint) {
@@ -35,4 +42,9 @@ contract Counter {
     if(!comments[commentId].exists) return 12345;
     return comments[commentId].down;
   }
+  function getCommentOwner(string memory commentId) public view returns (string memory){
+    if(!comments[commentId].exists) return "NO OWNER";
+    return string(abi.encodePacked(comments[commentId].creator));
+  }
+
 }
